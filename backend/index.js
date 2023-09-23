f036bcf3ced82e6816e2332daa26e71a322f1c74
bkpntx <bkpntx@gmail.com>
2023-09-22 23:16:27 -0500

    The whole thing

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,62 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = 3000; // May need to change PORT to something else if 3000 is already in use
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const { randomBytes } = require('crypto');
+let count =0;
+let allWords = [];
+let current = "";
+let currentFirst = "";
+let scrambled ="";
+let plays = 0;
+let prevWord = "";
+let score =0;
 
+fs.createReadStream("./english-words.csv")
+  .pipe(parse({ delimiter: ",", from_line: 1 }))
+  .on("data", function (row) {
+    allWords.push(row);
+    count++;
+  })
+  .on("end", function () {
+    console.log("finished");
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
+
+  function scramble(word){
+    strArr = [];
+    copy = word[0];
+    final = "";
+    for(i=0;i<copy.length;i++){
+      strArr.push(copy[i]);
+    }
+    for(i=0;i<copy.length;i++){
+      rand = Math.floor(Math.random()*copy.length);
+      hold = strArr[i];
+      strArr[i]=strArr[rand];
+      strArr[rand]= hold;
+    }
+    for(i=0;i<copy.length;i++){
+      final = final + strArr[i];
+    }
+    return final;
+  }
 app.use(cors());
 
-let score = 0;
-
-app.get('/hello', (req, res) => {
-    res.send('hello world!');
-});
 
 app.get('/score', (req, res) => {
     res.send(`${score}`);
+});
+
+app.get('/current', (req, res) => {
+  res.send(`${current}`);
+});
+
+app.get('/plays', (req, res) => {
+  res.send(`${plays}`);
 });
 
 app.patch('/score', (req, res) => {
@@ -21,12 +66,20 @@ app.patch('/score', (req, res) => {
 })
 
 app.get('/getWord', (req, res) => {
-    res.send('world');
+  prevWord = current;
+    let rand = Math.random() * count;
+    rand = Math.floor(rand);
+    current = allWords[rand];
+    scrambled = scramble(current);;
+    res.status(200).send(scrambled);
 })
 
 app.patch('/guessWord', (req, res) => {
-    if(req.query.word === 'world') {
+    plays++;
+    console.log("word:" + prevWord);
+    if(req.query.word == prevWord) {
         res.status(200).send('true');
+        score++;
     } else {
         res.status(200).send('false');
     }
--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -3,11 +3,14 @@ import { Input, Button, notification } from 'antd';
 import axios from 'axios';
 
 function Game () {
-
-    const [word, setWord] = useState("hello");
+9
+    const [word, setWord] = useState("");
     const [score, setScore] = useState(0);
     const [guess, setGuess] = useState("");
-
+    const [plays, setPlays] = useState(0);
+    const [hint, setHint]= useState("");
+    const [current, setCurrent]= useState("_");
+    
     useEffect(() => { 
         let config = {
             method: 'get',
@@ -23,20 +26,34 @@ function Game () {
             .catch((error) => {
                 console.log(error);
             });
-    });
+    }, []);
 
     const handleSubmit = () => {
         let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:3000/getWord',
+            headers: { }
+          };
+        
+          axios.request(config)
+            .then((response) => {
+                setWord(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        let config2 = {
         method: 'patch',
         maxBodyLength: Infinity,
         url: `http://localhost:3000/guessWord?word=${guess}`,
         headers: { }
         };
         
-        axios.request(config)
+        axios.request(config2)
             .then((response) => {
+                setScore;
                 if(response.data === true) {
-                    setScore(score + 1);
                     notification.success({
                         message: "Correct!",
                         description: "You guessed the word correctly!",
@@ -58,15 +75,69 @@ function Game () {
             });
     };
 
-    return <div className="card">
-        <h2> Current Word: {word} </h2>
-        <Input size="large" placeholder="Enter your guess"
-            onChange={(input) => {setGuess(input.target.value); }}
-            value={guess} />
-        <br /> <br />
-        <Button type="primary" size="large" onClick={handleSubmit}>Submit</Button>
-        <p> Score: {score} </p>
-    </div>
+    const handleSubmit2 = () => {
+        let config3 = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:3000/score',
+            headers: { }
+        };
+    
+        axios.request(config3)
+            .then((response) => {
+                setScore(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        let config4 = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:3000/plays',
+            headers: { }
+        };
+    
+        axios.request(config4)
+            .then((response) => {
+                setPlays(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    const handleHint = () => {
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: 'http://localhost:3000/current',
+            headers: { }
+        };
+    
+        axios.request(config)
+            .then((response) => {
+                setCurrent(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    return (
+        <div className="card">
+            <h2> Current Word: {word} </h2>
+            <Input size="large" placeholder="Enter your guess"
+                onChange={(input) => {setGuess(input.target.value); }}
+                value={guess} />
+            <br /> <br />
+            <Button type="primary" size="large" onClick={handleSubmit}>Submit</Button>
+            <br /> <br />
+            <Button type="primary" size="large" onClick={handleSubmit2}>Update Score</Button>
+            <br /> <br />
+            <Button type="primary" size="large" onClick={handleHint}>Update Hint</Button>
+            <p> Score: {score} </p>
+            <p> Rounds Played: {plays} </p>
+            <p> Hint Prompt: The first letter of the word is {current.charAt(0)} </p>
+        </div>
+    )
     
     
 }
